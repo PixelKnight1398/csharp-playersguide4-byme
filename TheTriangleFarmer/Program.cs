e21a3b129cde10f1c8676536e321f8dbbaab3e63
pixelknightt <darkhorizon1398@gmail.com>
2022-10-07 21:55:59 -0600

    Adding TheTriangleFarmer
    The Triangle Farmer project.

---
+++ TheTriangleFarmer/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TheTriangleFarmer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the triangle base?");
+            double triBase = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What is the triangle height?");
+            double triHeight = Convert.ToDouble(Console.ReadLine());
+
+            double area = (triBase * triHeight) / 2;
+            Console.WriteLine("Area of triangle is: ");
+            Console.WriteLine(area);
+        }
+    }
+}
