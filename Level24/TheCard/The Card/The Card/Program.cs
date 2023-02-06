5246e8177de70df660654c14282642382406bba0
PixelKnight1398 <darkhorizon1398@gmail.com>
2023-02-05 23:19:56 -0700

    Level 24 additions
    Added a few boss battles to the level 24

---
+++ Level24/TheCard/The Card/The Card/Program.cs
@@ -0,0 +1,61 @@
+﻿
+void Main()
+{
+    Card[] cardArray = new Card[56];
+    int cardIncrement = 0;
+    foreach(CardColors color in Card.CardColorArray)
+    {
+        foreach(CardRanks rank in Card.CardRankArray)
+        {
+            Card newCard = new Card(color, rank);
+            Console.WriteLine(newCard.cardDescription());
+            cardIncrement++;
+        }
+    }
+}
+
+Main();
+
+class Card
+{
+    public CardColors CardColor { get; set; }
+    public CardRanks CardRank { get; set; }
+
+    public static CardColors[] CardColorArray = new CardColors[4] { CardColors.Red, CardColors.Green, CardColors.Blue, CardColors.Yellow };
+    public static CardRanks[] CardRankArray = new CardRanks[14] { CardRanks.One, CardRanks.Two, CardRanks.Three, CardRanks.Four, CardRanks.Five, CardRanks.Six, CardRanks.Seven, CardRanks.Eight, CardRanks.Nine, CardRanks.Ten, CardRanks.Dollar, CardRanks.Percent, CardRanks.Caret, CardRanks.Ampersand };
+
+    public Card(CardColors cardColor, CardRanks cardRank) {
+        CardColor = cardColor;
+        CardRank = cardRank;
+    }
+
+    public bool isNumberCard()
+    {
+        return CardRank switch
+        {
+            CardRanks.One => true,
+            CardRanks.Two => true,
+            CardRanks.Three => true,
+            CardRanks.Four => true,
+            CardRanks.Five => true,
+            CardRanks.Six => true,
+            CardRanks.Seven => true,
+            CardRanks.Eight => true,
+            CardRanks.Nine => true,
+            CardRanks.Ten => true,
+            CardRanks.Dollar => false,
+            CardRanks.Percent => false,
+            CardRanks.Caret => false,
+            CardRanks.Ampersand => false,
+            _ => false
+        };
+    }
+
+    public string cardDescription()
+    {
+        return $"The {CardColor} {CardRank}";
+    }
+}
+
+enum CardColors { Red, Green, Blue, Yellow }
+enum CardRanks { One, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, Dollar, Percent, Caret, Ampersand }
---
+++ Level24/TheColor/The Color/The Color/Program.cs
@@ -0,0 +1,40 @@
+﻿void Main()
+{
+    Color color1 = new Color(0, 100, 200);
+    Color color2 = Color.Yellow;
+
+    Console.WriteLine(color1.displayColorValues());
+    Console.WriteLine(color2.displayColorValues());
+}
+
+Main();
+
+class Color
+{
+    public int RedValue { get; set; }
+    public int GreenValue { get; set; }
+    public int BlueValue { get; set; }
+
+    public Color() : this(0, 0, 0) { }
+
+    public Color(int red, int green, int blue)
+    {
+        RedValue = red;
+        GreenValue = green;
+        BlueValue = blue;
+    }
+
+    public string displayColorValues()
+    {
+        return $"({RedValue}, {GreenValue}, {BlueValue})";
+    }
+
+    public static Color White => new Color(255, 255, 255);
+    public static Color Black => new Color(0, 0, 0);
+    public static Color Red => new Color(255, 0, 0);
+    public static Color Orange => new Color(255, 165, 0);
+    public static Color Yellow => new Color(255, 255, 0);
+    public static Color Green => new Color(0, 128, 0);
+    public static Color Blue => new Color(0, 0, 255);
+    public static Color Purple => new Color(128, 0, 128);
+}
