fc3bea319e9fae30ebcce33bbdc0de6aeee3e5ba
PixelKnight1398 <darkhorizon1398@gmail.com>
2023-02-18 23:19:03 -0700

    Added chapters
    Added chapter 24, 25, 26

---
+++ Level26/LabelingInventory/Labeling Inventory/Labeling Inventory/Program.cs
@@ -0,0 +1,173 @@
+﻿void Main()
+{
+    Pack backpack = new Pack(20, 20f, 20);
+
+    while (true)
+    {
+        Console.WriteLine($"Pack weight: {backpack.CurrentWeight} - Pack volume: {backpack.CurrentVolume}");
+
+        Console.WriteLine("Which item do you want to add to the pack?");
+        Console.WriteLine("1 - Arrow");
+        Console.WriteLine("2 - Bow");
+        Console.WriteLine("3 - Rope");
+        Console.WriteLine("4 - Water");
+        Console.WriteLine("5 - Food Rations");
+        Console.WriteLine("6 - Sword");
+
+        int playerInput = Int32.Parse(Console.ReadLine());
+
+        InventoryItem addedItem = playerInput switch
+        {
+            1 => new Arrow(),
+            2 => new Bow(),
+            3 => new Rope(),
+            4 => new Water(),
+            5 => new Food(),
+            6 => new Sword(),
+        };
+
+        if (backpack.Add(addedItem))
+            Console.WriteLine("Item added successfully");
+        else
+            Console.WriteLine("Pack is too full for that item");
+
+        Console.WriteLine($"New pack weight: {backpack.CurrentWeight} - New pack volume: {backpack.CurrentVolume}");
+        Console.WriteLine(backpack.ToString());
+    }
+}
+
+Main();
+
+class Pack
+{
+    public InventoryItem[] Items { get; set; }
+    public float CurrentWeight { get; set; }
+    public float MaximumWeight { get; set; }
+    public float CurrentVolume { get; set; }
+    public float MaximumVolume { get; set; }
+
+    public Pack(int itemCapacity, float maximumWeight, float maximumVolume)
+    {
+        Items = new InventoryItem[itemCapacity];
+        MaximumWeight = maximumWeight;
+        MaximumVolume = maximumVolume;
+    }
+
+    public bool Add(InventoryItem item)
+    {
+        RecalculatePackInfo();
+
+        if ((CurrentWeight + item.Weight) > MaximumWeight || (CurrentVolume + item.Volume) > MaximumVolume)
+            return false;
+        else
+        {
+            for (int i = 0; i < Items.Length; i++)
+            {
+                if (Items[i] == null)
+                {
+                    Items[i] = item;
+                    break;
+                }
+            }
+            return true;
+        }
+    }
+
+    public void RecalculatePackInfo()
+    {
+        CurrentWeight = 0;
+        CurrentVolume = 0;
+
+        foreach (InventoryItem zItem in Items)
+        {
+            if (zItem != null)
+            {
+                CurrentWeight += zItem.Weight;
+                CurrentVolume += zItem.Volume;
+            }
+        }
+    }
+
+    public override string ToString()
+    {
+        string objectString = "Pack contains:\n";
+
+        foreach(InventoryItem zItem in Items)
+        {
+            if(zItem != null)
+                objectString += $"\t-{zItem.ToString()}\n";
+        }
+
+        return objectString;
+    }
+}
+
+class InventoryItem
+{
+    public float Weight { get; set; }
+    public float Volume { get; set; }
+
+    public InventoryItem(float itemWeight, float itemVolume)
+    {
+        Weight = itemWeight;
+        Volume = itemVolume;
+    }
+}
+
+class Arrow : InventoryItem
+{
+    public Arrow() : base(0.1f, 0.05f) { }
+
+    public override string ToString()
+    {
+        return "Arrow";
+    }
+}
+
+class Bow : InventoryItem
+{
+    public Bow() : base(1f, 4f) { }
+
+    public override string ToString()
+    {
+        return "Bow";
+    }
+}
+
+class Rope : InventoryItem
+{
+    public Rope() : base(1f, 1.5f) { }
+
+    public override string ToString()
+    {
+        return "Rope";
+    }
+}
+
+class Water : InventoryItem
+{
+    public Water() : base(2f, 3f) { }
+
+    public override string ToString()
+    {
+        return "Water";
+    }
+}
+class Food : InventoryItem
+{
+    public Food() : base(1f, 0.5f) { }
+
+    public override string ToString()
+    {
+        return "Food";
+    }
+}
+class Sword : InventoryItem
+{
+    public Sword() : base(5f, 3f) { }
+
+    public override string ToString()
+    {
+        return "Sword";
+    }
+}
---
+++ Level26/TheOldRobot/The Old Robot/The Old Robot/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+void Main()
+{
+    RobotCommand[] commandInputs = new RobotCommand[3];
+
+    Console.WriteLine("Please enter a command for the robot:");
+    Console.WriteLine("[1] Power On");
+    Console.WriteLine("[2] Power Off");
+    Console.WriteLine("[3] Move North");
+    Console.WriteLine("[4] Move South");
+    Console.WriteLine("[5] Move East");
+    Console.WriteLine("[6] Move West");
+
+    for (int i = 0; i < 3; i++)
+    {
+        string commandTextInput = Console.ReadLine();
+        commandInputs[i] = commandTextInput switch
+        {
+            "1" => new OnCommand(),
+            "2" => new OffCommand(),
+            "3" => new NorthCommand(),
+            "4" => new SouthCommand(),
+            "5" => new EastCommand(),
+            "6" => new WestCommand()
+        };
+    }
+
+    Robot deployedRobot = new Robot(0, 0, false, commandInputs);
+    deployedRobot.Run();
+}
+
+Main();
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public RobotCommand[] Commands { get; } = new RobotCommand[3];
+
+    public Robot(int x, int y, bool isPowered, RobotCommand[] commands)
+    {
+        X = x;
+        Y = y;
+        IsPowered = isPowered;
+        Commands = commands;
+    }
+
+    public void Run()
+    {
+        foreach(RobotCommand command in Commands)
+        {
+            command.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public abstract class RobotCommand
+{
+    public abstract void Run(Robot robot);
+}
+
+public class OnCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.Y += 1;
+    }
+}
+
+public class SouthCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.Y -= 1;
+    }
+}
+
+public class WestCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.X -= 1;
+    }
+}
+
+public class EastCommand : RobotCommand
+{
+    public override void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.X += 1;
+    }
+}
