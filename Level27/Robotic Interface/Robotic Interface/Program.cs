2fd9dce96c9de3f0a22b1eeca5513bf25bd7a324
PixelKnight1398 <darkhorizon1398@gmail.com>
2023-02-19 13:31:54 -0700

    Added chapters
    Chapter 27/28

---
+++ Level27/Robotic Interface/Robotic Interface/Program.cs
@@ -0,0 +1,115 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+void Main()
+{
+    IRobotCommand[] commandInputs = new IRobotCommand[3];
+
+    Console.WriteLine("Please enter a command for the robot:");
+    Console.WriteLine("[1] Power On");
+    Console.WriteLine("[2] Power Off");
+    Console.WriteLine("[3] Move North");
+    Console.WriteLine("[4] Move South");
+    Console.WriteLine("[5] Move East");
+    Console.WriteLine("[6] Move West");
+
+    for (int i = 0; i < 3; i++)
+    {
+        string commandTextInput = Console.ReadLine();
+        commandInputs[i] = commandTextInput switch
+        {
+            "1" => new OnCommand(),
+            "2" => new OffCommand(),
+            "3" => new NorthCommand(),
+            "4" => new SouthCommand(),
+            "5" => new EastCommand(),
+            "6" => new WestCommand()
+        };
+    }
+
+    Robot deployedRobot = new Robot(0, 0, false, commandInputs);
+    deployedRobot.Run();
+}
+
+Main();
+
+public class Robot
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public bool IsPowered { get; set; }
+    public IRobotCommand[] Commands { get; } = new IRobotCommand[3];
+
+    public Robot(int x, int y, bool isPowered, IRobotCommand[] commands)
+    {
+        X = x;
+        Y = y;
+        IsPowered = isPowered;
+        Commands = commands;
+    }
+
+    public void Run()
+    {
+        foreach (IRobotCommand command in Commands)
+        {
+            command.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public interface IRobotCommand
+{
+    void Run(Robot robot);
+}
+
+public class OnCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = true;
+    }
+}
+
+public class OffCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        robot.IsPowered = false;
+    }
+}
+
+public class NorthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.Y += 1;
+    }
+}
+
+public class SouthCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.Y -= 1;
+    }
+}
+
+public class WestCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.X -= 1;
+    }
+}
+
+public class EastCommand : IRobotCommand
+{
+    public void Run(Robot robot)
+    {
+        if (robot.IsPowered)
+            robot.X += 1;
+    }
+}
---
+++ Level28/Room Coordinates/Room Coordinates/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Data;
+
+void Main()
+{
+    Coordinate[] coordinateArray = new Coordinate[10];
+    Random random = new Random();
+    for(int i = 0; i < coordinateArray.Length; i++)
+    {
+        coordinateArray[i] = new Coordinate(random.Next(6), random.Next(6));
+        Console.WriteLine($"{coordinateArray[i].Row}, {coordinateArray[i].Column}");
+    }
+
+    foreach(Coordinate coord in coordinateArray)
+        Console.WriteLine($"{coord.Row}, {coord.Column} coord has an adjacent coord: {coord.hasAdjacent(coordinateArray)}");
+}
+
+Main();
+
+public struct Coordinate
+{
+    public int Row { get; }
+    public int Column { get; }
+
+    public Coordinate(int row, int column)
+    {
+        Row = row;
+        Column = column;
+    }
+
+    public bool hasAdjacent(Coordinate[] coordArray)
+    {
+        foreach(Coordinate coord in coordArray)
+        {
+            if (((coord.Row == (Row - 1) || coord.Row == (Row + 1)) && coord.Column == Column) ||
+                ((coord.Column == Column - 1 || coord.Column == Column + 1) && coord.Row == Row))
+                return true;
+        }
+
+        return false;
+    }
+}
