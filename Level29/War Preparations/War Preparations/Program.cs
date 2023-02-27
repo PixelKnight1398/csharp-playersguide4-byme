83ea1d4c4808d997432f82296f3b289499d2478b
PixelKnight1398 <darkhorizon1398@gmail.com>
2023-02-26 18:52:59 -0700

    Added Chapters
    28, 29, 30

---
+++ Level29/War Preparations/War Preparations/Program.cs
@@ -0,0 +1,17 @@
+﻿void Main()
+{
+    Sword basicSword = new Sword(SwordMaterial.Iron, Gemstone.None, 40.0f, 5.0f);
+    Sword sapphireSword = basicSword with { Gem = Gemstone.Sapphire };
+    Sword conquererSword = basicSword with { Material = SwordMaterial.Steel, Gem = Gemstone.Bitstone, Length = 60.0f, CrossguardWidth = 10.0f };
+
+    Console.WriteLine(basicSword);
+    Console.WriteLine(sapphireSword);
+    Console.WriteLine(conquererSword);
+}
+
+Main();
+
+public record Sword (SwordMaterial Material, Gemstone Gem, float Length, float CrossguardWidth);
+
+public enum SwordMaterial { Wood, Bronze, Iron, Steel, Binarium }
+public enum Gemstone { None, Emerald, Amber, Sapphire, Diamond, Bitstone }
---
+++ Level30/Colored Items/Colored Items/Program.cs
@@ -0,0 +1,33 @@
+﻿void Main()
+{
+    ColoredItem<Sword> blueSword = new ColoredItem<Sword>(new Sword(), ConsoleColor.Blue);
+    blueSword.Display();
+    ColoredItem<Bow> redBow = new ColoredItem<Bow>(new Bow(), ConsoleColor.Red);
+    redBow.Display();
+    ColoredItem<Axe> greenAxe = new ColoredItem<Axe> (new Axe(), ConsoleColor.Green);
+    greenAxe.Display();
+}
+
+Main();
+
+public class ColoredItem<T> where T : class
+{
+    T Item { get; set; }
+    ConsoleColor Color { get; set; }
+
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item.ToString());
+    }
+}
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
